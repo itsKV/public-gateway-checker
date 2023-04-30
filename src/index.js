df4e12c3ac9821c10a1536edbbc269d7f5f60484
itsKV <itsKV@users.noreply.github.com>
2023-04-30 04:07:20 +0000

    Update gh-pages to output generated at a111796

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { Checker } from './Checker';
 import gateways from './gateways.json';
+import { loadCountly } from './metrics';
 import { Log } from './Log';
+import { Workbox } from 'workbox-window';
+const wb = new Workbox('/sw.js');
+void wb.register();
+loadCountly();
 const log = new Log('App index');
 window.checker = new Checker();
 window.checker.checkGateways(gateways).catch((err) => {
